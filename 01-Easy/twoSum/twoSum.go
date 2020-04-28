63d977d441b9e8e1209963f87dbcecd6bbcb6820
chrisfranz <chrisfranz@mac.com>
2020-04-27 21:50:15 -0700

    added twoSum with sorted array

--- 01-Easy/twoSum/twoSum.go
+++ 01-Easy/twoSum/twoSum.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func twoSum(array []int, target int) []int {
 	nums := map[int]bool{}
@@ -15,6 +18,32 @@ func twoSum(array []int, target int) []int {
 	return []int{};
 }
 
+func twoSumSorted(array []int, target int) []int {
+	sort.Ints(array)
+
+	left := 0
+	right := len(array) - 1
+	
+	for left < right {
+		leftVal := array[left]
+		rightVal := array[right]
+		sum := leftVal + rightVal
+
+		if sum == target {
+			return []int{ leftVal, rightVal}
+		} else if sum < target { 
+			left++
+		} else {
+			right--
+		}
+	}
+	return[]int{}
+}
+
+// var output1  = twoSum([]int{7, 6, 5, 4}, 13)
+
+
 func main() {
-	fmt.Println(twoSum([]int{7,6,5,4}, 13))
+	var output2  = twoSumSorted([]int{7, 6, 5, 4, -20, 234, 9, 22, -7}, -1690)
+	fmt.Println(output2)
 }
